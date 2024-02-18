bea35940a93a6fafcfb40875a02a1bcc4acb7782
Sheryar Kayani <sheryarali2010@gmail.com>
2024-02-18 11:30:01 +0500

    complete app

---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,46 @@
+import { Sidebar } from "flowbite-react";
+import { AiOutlineCompass } from "react-icons/ai";
+import { MdOutlineDashboard } from "react-icons/md";
+import { BiBuoy } from "react-icons/bi";
+import AppNavLink from "../navLink";
+import { useStateContext } from "../../context";
+import logoImage from "../../assets/logo.png";
+
+export default function SideBar() {
+    const {navbarShrink} = useStateContext();
+  return (
+    <Sidebar className={`${navbarShrink ? 'w-[4rem]' : ''} side-bar h-screen sticky top-0 left-0 bottom-0 shadow border-r border-[gainsboro] dark:border-r dark:border-gray-700`}>
+      <Sidebar.Logo
+      className="sidebar-logo"
+        href="/"
+      >
+      <div className="flex items-center">
+        <img width={40} src={logoImage} style={{filter: "invert(1)"}} className="dark:hidden" alt="Your Company Logo" />
+        <img width={40} src={logoImage} className="dark:block hidden" alt="Your Company Logo" />
+  
+      {!navbarShrink &&
+        <p className="ml-2 text-sm">Avenue5 Overview </p>
+      }
+      </div>
+      </Sidebar.Logo>
+      <Sidebar.Items className="flex flex-col h-[90%]">
+        <Sidebar.ItemGroup className="flex-1">
+          <AppNavLink icon={AiOutlineCompass} to={"/"} text={"Explore"} />
+          <AppNavLink
+            icon={MdOutlineDashboard}
+            to={"/dashboard"}
+            text={"Dashboard"}
+          />
+        </Sidebar.ItemGroup>
+        <Sidebar.ItemGroup>
+          <AppNavLink
+            icon={BiBuoy}
+            to={"/help"}
+            text={"Help"}
+          />
+        
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
